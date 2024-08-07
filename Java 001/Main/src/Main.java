43ec47dc4711f75e998f98bb45cc4e945f1f03c9
Giovanni Pascuotte da Silva <100391366+gean12390@users.noreply.github.com>
2024-08-07 15:24:13 -0300

    Add files via upload

---
+++ Java 001/Main/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+  public static void main(String[] args) {
+    int x = 5;
+    System.out.println(x);
+    System.out.println(3 + 4);
+    System.out.println(12 < 7);
+    System.out.println(12 > 7);
+    System.out.println(12 == 7);
+    System.out.println(12 <= 7);
+    System.out.println(12 >= 7);
+    System.out.println(!(2 == 2));
+    System.out.println("Hello World!!");
+
+        if(x == 5){
+              System.out.println("X é igual a 5");
+        } else if(x < 5){
+                System.out.println("X Não é maior que 5");
+          } else{
+              System.out.println("X é diferente de 5");
+        } 
+    
+  }
+}
