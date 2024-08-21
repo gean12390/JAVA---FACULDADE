77f06ddb104e1b2bd7d1723b1e1750c9c13581b5
Giovanni Pascuotte da Silva <100391366+gean12390@users.noreply.github.com>
2024-08-21 10:19:11 -0300

    Create Main.java

---
+++ java003/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static boolean teste(int n1){
+    
+        if(n1 % 2 == 0)
+            return true;
+        
+        else 
+            return false;
+        
+    }
+	public static void main(String[] args) {
+		System.out.println(teste(5));
+	}
+}
